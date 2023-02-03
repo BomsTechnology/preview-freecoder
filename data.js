744f86f9830babab41c921fcd4b2881dbac1b264
Marcelin Sigha <86481254+BomsTechnology@users.noreply.github.com>
2023-02-03 13:51:41 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,308 @@
+export default [
+    {
+        question: 'Pourquoi faire mon diagnostic cappillaire ?',
+        question_id: 'pourquoi_faire_mon_diagnostic_cappillaire',
+        answer_type: '',
+        answers: [
+            {
+                type:'text', 
+                path:'', 
+                value:'C\'est 100% gratuit',
+                info: '',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Rapide (ça prend 5 minutes)',
+                info: '',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'100% naturels',
+                info: '',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Vegan',
+                info: '',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Routine simple et personnalisée',
+                info: '',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Made in France',
+                info: '',
+            }
+        ],
+        button_text: 'Obtenir mon diagnostic',
+    },
+    {
+        question: 'Choix du genre',
+        question_id: 'choix_du_genre',
+        answer_type: 'single',
+        answers: [
+            {
+                type: 'text',
+                path:'',
+                value: 'Femme',
+                info: '',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Homme',
+                info:''
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Non Binaire',
+                info:''
+            },
+        ],
+        button_text: 'Confirmer',
+    },
+    {
+        question: 'Aspect du cheuveux',
+        question_id: 'aspect_du_cheuveux',
+        answer_type: 'single',
+        answers: [
+            {
+                type: 'text',
+                path: '',
+                value: 'Rapides',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Lisses',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Ondulés',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type: 'text',
+                path:'',
+                value: 'Crépus',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Frisés',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Bouclés',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+        ],
+        button_text: 'Confirmer',
+    },
+    {
+        question: 'Structure des cheuveux',
+        question_id: 'structure_des_cheuveux',
+        answer_type: 'multiple',
+        answers: [
+            {
+                type: 'text',
+                path:'',
+                value: 'Secs',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Fins',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Abimés',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type: 'text',
+                path:'',
+                value: 'Parcemés',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Fourchus',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Cassants',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type: 'text',
+                path:'',
+                value: 'Epais',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Gras',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Dévitalisés',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Colorés',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+        ],
+        button_text: 'Confirmer',
+    },
+    {
+        question: 'Je voudrais que mon produit',
+        question_id: 'je_voudrais_que_mon_produit',
+        answer_type: 'multiple',
+        answers: [
+            {
+                type: 'text',
+                path:'',
+                value: 'Hydrate',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Lisse',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Illumine',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type: 'text',
+                path:'',
+                value: 'Fortifie',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Repare en profondeur',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Donne du volume',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type: 'text',
+                path:'',
+                value: 'Nourrit',
+                info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo',
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Aide à la repousse',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Anti-pélicullaire',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Revitalise',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'',
+                value:'Ravive ma couleur',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+            {
+                type:'text', 
+                path:'', 
+                value:'Anti-cheveux gras',
+                info:'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur reiciendis aperiam unde explicabo'
+            },
+        ],
+        button_text: 'Confirmer',
+    },
+    {
+        question: 'Ma senteur préféré',
+        question_id: 'ma_senteur_prefere',
+        answer_type: 'single',
+        answers: [
+            {
+                type: 'image',
+                path: 'https://oudandmusk.fr/2013-home_default/oliban-d-oman-resine-encens-louban-dakar.jpg',
+                value: 'Musc Blanc',
+                info: '',
+            },
+            {
+                type:'image', 
+                path:'https://www.tastingtable.com/img/gallery/which-country-produces-the-most-coconuts/intro-1661971699.webp',
+                value:'Coconut',
+                info:''
+            },
+            {
+                type:'image', 
+                path:'https://nanoil.us/assets/uploads/2019/06/60315fbea2d2f6f8668eb1c1694c7bfe.jpg',
+                value:'Monoi',
+                info:''
+            },
+            {
+                type: 'image',
+                path:'https://i0.wp.com/bonpourtoi.ca/app/uploads/2021/06/BPT-Articles-fraises-quebec-tinyjpg-2.jpeg?fit=2530%2C1860&ssl=1',
+                value: 'Fraise',
+                info: '',
+            },
+            {
+                type:'image', 
+                path:'https://ichef.bbci.co.uk/news/800/cpsprodpb/1884/live/0ec843d0-6110-11ed-b950-4dadc68f0cfc.jpg',
+                value:'Banane',
+                info:'',
+            },
+            {
+                type:'image', 
+                path:'https://www.radiofrance.fr/s3/cruiser-production/2022/02/c8cf1de7-a946-4964-bc73-7c324b89326b/870x489_gettyimages-135607225.webp',
+                value:'Ananas',
+                info:'',
+            },
+        ],
+        button_text: 'J\'obtiens mon diagnostic',
+    },
+]
