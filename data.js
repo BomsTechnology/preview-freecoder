3579633980509ad6f8e4bfd39c7a8c8dc0f8f09e
Marcelin Sigha <86481254+BomsTechnology@users.noreply.github.com>
2023-03-13 23:39:49 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,39 +1,58 @@
-let sizes = [ { "hauteur":"1940", "largeur":"1000", "prix":"297", "id":"44619369971998" }, { "hauteur":"1940", "largeur":"1300", "prix":"308", "id":"44619370004766" }, { "hauteur":"1940", "largeur":"1600", "prix":"331", "id":"44619370037534" }, { "hauteur":"1940", "largeur":"1900", "prix":"0", "id":"44619370070302" }, { "hauteur":"2091", "largeur":"1000", "prix":"311", "id":"44619370103070" }, { "hauteur":"2091", "largeur":"1300", "prix":"322", "id":"44619370135838" }, { "hauteur":"2091", "largeur":"1600", "prix":"345", "id":"44619370168606" }, { "hauteur":"2091", "largeur":"1900", "prix":"368", "id":"44619370201374" }, { "hauteur":"2241", "largeur":"1000", "prix":"324", "id":"44619370234142" }, { "hauteur":"2241", "largeur":"1300", "prix":"335", "id":"44619370266910" }, { "hauteur":"2241", "largeur":"1600", "prix":"358", "id":"44619370299678" }, { "hauteur":"2241", "largeur":"1900", "prix":"381", "id":"44619370332446" }, { "hauteur":"2421", "largeur":"1000", "prix":"338", "id":"44619370365214" }, { "hauteur":"2421", "largeur":"1300", "prix":"349", "id":"44619370397982" }, { "hauteur":"2421", "largeur":"1600", "prix":"372", "id":"44619370430750" }, { "hauteur":"2421", "largeur":"1900", "prix":"394", "id":"44619370463518" }, { "hauteur":"2571", "largeur":"1000", "prix":"351", "id":"44619370529054" }, { "hauteur":"2571", "largeur":"1300", "prix":"362", "id":"44619370594590" }, { "hauteur":"2571", "largeur":"1600", "prix":"385", "id":"44619370627358" }, { "hauteur":"2571", "largeur":"1900", "prix":"408", "id":"44619370660126" }, { "hauteur":"2701", "largeur":"1000", "prix":"378", "id":"44619370692894" }, { "hauteur":"2701", "largeur":"1300", "prix":"389", "id":"44619370725662" }, { "hauteur":"2701", "largeur":"1600", "prix":"412", "id":"44619370758430" }, { "hauteur":"2701", "largeur":"1900", "prix":"0", "id":"44619370791198" }, ];
-
-let poseTypes = [
+export default  [
     {
-        "id": "tunnel",
-        "value": 100,
-        "image": "https://www.promoustiquaire.com/img/co/178.jpg",
-        "label": "En tunnel (dans le tableau)",
+        "id": '1',
+        "title": 'Baby Boomer',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/IMG_8404_700x.png?v=1661102061',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/Creamer_700x.jpg?v=1661102061',
+        "color": '#ecf0f1', 
     },
     {
-        "id": "applique",
-        "value": 200,
-        "image": "https://www.promoustiquaire.com/img/co/179.jpg",
-        "label": "En applique (pose frontale)",
+        "id": '2',
+        "title": 'Aura',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/HNB-FICHEPRODUIT_9a5f7c3f-2da2-4b53-90c5-7f96b3a12232_700x.png?v=1661083781',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/1039413C-1887-4032-84AC-B5126DF4714F_700x.jpg?v=1661083781',
+        "color": '#dff9fb', 
+    },
+    {
+        "id": '3',
+        "title": 'Baby Purple',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/HNB-FICHEPRODUIT_97de9612-ec94-430d-9339-f6a2edc85b11_700x.png?v=1658131803',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/DSC01466copie_700x.jpg?v=1658131803',
+        "color": '#7ed6df', 
+    },
+    {
+        "id": '4',
+        "title": 'Baby Ocean',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/HNB-FICHEPRODUIT_4_700x.png?v=1655370224',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/DSC01464_f5845475-87dd-462c-9e21-8164a28e0ca8_700x.jpg?v=1655406779',
+        "color": '#d2dae2', 
+    },
+    {
+        "id": '5',
+        "title": 'Chocolate Chip',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/92_700x.png?v=1675869876',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/80_700x.png?v=1676202981',
+        "color": '#fab1a0', 
+    },
+    {
+        "id": '6',
+        "title": 'Cloud Nine',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/HNBphotocontent_5_700x.png?v=1675869953',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/HNBphotocontent_6_700x.png?v=1676202986',
+        "color": '#81ecec', 
+    },
+    {
+        "id": '7',
+        "title": 'Coffee Date',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/79_700x.png?v=1675786401',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/HNBphotocontent_700x.png?v=1675786543',
+        "color": '#e17055', 
+    },
+    {
+        "id": '8',
+        "title": 'Green Peace',
+        "featured_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/IMG_5512_700x.jpg?v=1655370253',
+        "hover_image": 'https://cdn.shopify.com/s/files/1/0590/2948/7803/products/DSC01456_700x.jpg?v=1655406820',
+        "color": '#55efc4', 
     },
 ];
-
-let coloris = [
-    {
-        "id": "blanc",
-        "value": 100,
-        "image": "https://www.promoustiquaire.com/img/co/26.jpg",
-        "label": "Blanc",
-    },
-    {
-        "id": "standard",
-        "value": 200,
-        "image": "https://www.promoustiquaire.com/img/co/29.jpg",
-        "label": "Coloris Standard",
-    },
-    {
-        "id": "imitationbois",
-        "value": 300,
-        "image": "https://www.promoustiquaire.com/img/co/30.jpg",
-        "label": "Imitation bois",
-    },
-];
-
-export { sizes, poseTypes, coloris };
