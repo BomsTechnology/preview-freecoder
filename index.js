1f454520a9655d379cc3d3d632fb1e8413fd1c74
Marcelin Sigha <86481254+BomsTechnology@users.noreply.github.com>
2023-03-13 23:56:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -70,9 +70,9 @@ function renderSelectedProducts () {
         html +=  productsSelected[i] ? ` 
             <div class="product_selected active">
                 <span class="remove_btn">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>                                      
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>                                                   
                 </span>
                 <img src="${productsSelected[i].featured_image}" class="featured_image" alt="">
             </div>` : 
